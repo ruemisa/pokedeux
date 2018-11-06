e3b5872b1f274095b47b095fdaacf619a6f60507
Ruel Alfonso Misa <ruel.g.misa@gmail.com>
2018-11-06 10:11:06 -0500

    integrated babel, webpack dev server, and updated config

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
 import x from './test';
 
 console.log(`i imported ${x}`);
+console.log(`testme`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,34 @@
 // Basic config setup for webpack bundle
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/js/index.js',
+    entry: ['babel-polyfill', './src/js/index.js'],
     output: {
-        path: path.resolve(__dirname, 'dist/js'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
     },
+    // Webpack Dev Server
+    devServer: {
+        contentBase: './dist'
+    },
+    // Plugins
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    // Babel
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
 };
